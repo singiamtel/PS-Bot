fd5c2ef18b373805680cef84e6c6b3f75893d4d3
Sergio <sergio@garciadelacruz.es>
2024-01-20 22:04:10 +0100

    Fix old randttp

--- src/mods/ttp.ts
+++ src/mods/ttp.ts
@@ -7,7 +7,7 @@ export function ttp(message: Message) {
         const num = message.content.split(' ')[1];
         if (!num) return message.reply('Please specify a card number.');
         message.reply(`!show ${TTPurl}card${num}.png`);
-    } else if (/#randttp /.test(message.content)) {
+    } else if (/#randttp$/.test(message.content)) {
     // cards are between 1 and 52
         const num = Math.floor(Math.random() * 52) + 1;
         message.reply(`!show ${TTPurl}card${num}.png, Card #${num}`);
