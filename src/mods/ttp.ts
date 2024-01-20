92a1ccb987f277be9ee9cf53642ec318c0591ffe
Sergio <sergio@garciadelacruz.es>
2024-01-20 21:54:40 +0100

    TTP2

--- src/mods/ttp.ts
+++ src/mods/ttp.ts
@@ -13,3 +13,17 @@ export function ttp(message: Message) {
         message.reply(`!show ${TTPurl}card${num}.png, Card #${num}`);
     }
 }
+
+const TTP2url = process.env.TTP2url || 'https://home.showcord.com/TTP2/';
+
+export function ttp2(message: Message) {
+    if (/#ttp2/.test(message.content)) {
+        const num = message.content.split(' ')[1];
+        if (!num) return message.reply('Please specify a card number.');
+        message.reply(`!show ${TTP2url}card${num}.png`);
+    } else if (/#randttp/.test(message.content)) {
+    // cards are between 1 and 52
+        const num = Math.floor(Math.random() * 32) + 1;
+        message.reply(`!show ${TTP2url}card${num}.png, Card #${num}`);
+    }
+}
