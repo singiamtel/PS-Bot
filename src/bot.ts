755a4be3e25371e6ead0e7d414f3e84a0a381a8b
Sergio <sergio@garciadelacruz.es>
2024-02-05 23:46:47 +0100

    Fix

--- src/bot.ts
+++ src/bot.ts
@@ -57,7 +57,7 @@ export function isAuth(message: Message, room?: string) {
     if (room) {
         const authObject = client.getRoom(room)?.auth;
         if (authObject) {
-            const authList = Object.entries(client.getRoom(room).auth).filter(([rank, _userArray]) => rankOrder[rank as keyof typeof rankOrder] > 2).map(([_rank, userArray]) => userArray).flat();
+            const authList = Object.entries(client.getRoom(room).auth).filter(([rank, _userArray]) => rankOrder[rank as keyof typeof rankOrder] > 4).map(([_rank, userArray]) => userArray).flat();
             return authList.includes(toID(message.author?.name));
         } else {
             return false;
