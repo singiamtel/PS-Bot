e5dae0827119952e2a5261505b2b8c4b511cba67
Sergio <sergio@garciadelacruz.es>
2024-02-09 13:20:41 +0100

    ...

--- src/mods/randopple.ts
+++ src/mods/randopple.ts
@@ -7,6 +7,6 @@ const nOpples = 25;
 export function randopple(message:Message) {
     if (isCmd(message, 'randopple')) {
         const num = Math.floor(Math.random() * nOpples) + 1;
-        message.reply(`!show ${TTPurl}{num}.png`);
+        message.reply(`!show ${TTPurl}${num}.png`);
     }
 }
