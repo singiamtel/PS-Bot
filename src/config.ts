452eba84b58870a1328796ec7696200c6f418593
Sergio <sergio@garciadelacruz.es>
2024-08-30 19:06:31 +0200

    Fix env

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export const rootDir = __dirname;
+
+dotenv.config({ path: path.join(rootDir, '../.env') });
 
 
 let __config = {
